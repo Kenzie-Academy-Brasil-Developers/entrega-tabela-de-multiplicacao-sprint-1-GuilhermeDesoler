72af114fda8db586196be278a9336cd834fb3f7d
guilherme <guilherme.desoler@gmail.com>
2021-08-11 18:06:34 -0300

    Adding the index.html and the script.js file with the Table function.

---
+++ script.js
@@ -0,0 +1,20 @@
+/////////////////////////////////////////// OK
+// Function MultiplyTable //
+////////////////////////////
+
+function multiplyTable(size) {
+    // Hello, like the first step of the exercice i did a 10x10 table by default.
+    if(size === undefined){  
+        size = 10
+    }
+    let vertical = []                               // Starting the vertical axis.
+    for (let i = 0; i <= size; i++) {               // Doing a for that defines the size of the table.
+        let horizontal = []                         // Creating the horizontal axis.
+        for(let j = 0; j <=size; j++){              // Doing a for multiply each cell.
+            horizontal[j] = j * i                   // The multiplication
+        }
+        vertical[i] = horizontal                    // Adding the horizontal array
+    }
+    console.table(vertical)
+}
+/////////////////////////////////////////
